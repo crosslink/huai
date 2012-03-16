d22d0905bc451e67a54e9926fd0f6ee23dd1b116
CN Sturgeon <cnsturgeon2000@yahoo.com>
2012-03-16 12:13:08 +1000

    init import

---
+++ templates/shape5_vertex/vertex/footer.php
@@ -0,0 +1,26 @@
+<?php
+
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted index access' );
+
+global $mainframe;
+
+jimport('joomla.utilities.date');
+$app = JFactory::getApplication();
+
+$date = new JDate();
+$cur_year	= $date->toFormat('%Y');
+$csite_name	= $app->getCfg('sitename');
+
+// NOTE - You may change this file to suit your site needs
+?>
+<span class="footerc">
+Copyright &copy; <?php echo $cur_year;?>.  <?php echo $csite_name;?>.
+Designed by Shape5.com <a href="http://www.shape5.com/" class="footerc" title="Visit Shape5.com Joomla Template Club" target="blank">Joomla Templates</a>
+</span>
+
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F4f4c315bc7f692b0e7aa72bf2ed450aa' type='text/javascript'%3E%3C/script%3E"));
+</script>
