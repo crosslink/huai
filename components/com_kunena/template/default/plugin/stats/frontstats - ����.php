64b99faa04ae898945d1d41a8ac891dd5aa28c0a
Eric Tang <lingxiang.tang@gmail.com>
2012-04-12 10:55:06 -0400

    added other files

---
+++ components/com_kunena/template/default/plugin/stats/frontstats - ����.php
@@ -0,0 +1,48 @@
+<?php
+
+
+// Dont allow direct linking
+defined( '_JEXEC' ) or die();
+
+if ($this->showgenstats > 0)
+{
+	$this->loadGenStats();
+
+	$kunena_config = KunenaFactory::getConfig ();
+
+	$userlist1 = CKunenaLink::GetUserlistLink('', $this->totalmembers);
+	$userlist2 = CKunenaLink::GetUserlistLink('', JText::_('COM_KUNENA_STAT_USERLIST').' &raquo;');
+
+	?>
+<div class="kblock kfrontstats">
+	<div class="kheader">
+		<span class="ktoggler"><a class="ktoggler close" title="<?php echo JText::_('COM_KUNENA_TOGGLER_COLLAPSE') ?>" rel="kfrontstats-tbody"></a></span>
+	<h2><span><?php echo CKunenaLink::GetStatsLink(JText::_('COM_KUNENA_STAT_FORUMSTATS'), ''); ?></span></h2>
+	</div>
+	<div class="kcontainer" id="kfrontstats-tbody">
+		<div class="kbody">
+			<table class = "kblocktable" id ="kfrontstats">
+				<tr class="krow1">
+					<td class = "kcol-first">
+						<div class="kstatsicon"></div>
+					</td>
+					<td class="kcol-mid km">
+						<ul id="kstatslistright" class="fltrt kright">
+							<li><?php echo JText::_('COM_KUNENA_STAT_TOTAL_USERS'); ?>: <strong><?php echo $userlist1; ?></strong> <span class="divider">|</span> <?php echo JText::_('COM_KUNENA_STAT_LATEST_MEMBERS'); ?>:<strong> <?php echo CKunenaLink::GetProfileLink($this->lastestmemberid); ?></strong></li>
+							<li>&nbsp;</li>
+						</ul>
+						<ul id="kstatslistleft" class="fltlft">
+							<li><?php echo JText::_('COM_KUNENA_STAT_TOTAL_MESSAGES'); ?>: <strong> <?php echo intval($this->totalmsgs); ?></strong> <span class="divider">|</span> <?php echo JText::_('COM_KUNENA_STAT_TOTAL_SUBJECTS'); ?>: <strong><?php echo intval($this->totaltitles); ?></strong></li>
+							<li><?php echo JText::_('COM_KUNENA_STAT_TODAY_OPEN_THREAD'); ?>: <strong><?php echo intval($this->todayopen); ?></strong> <span class="divider">|</span> <?php echo JText::_('COM_KUNENA_STAT_YESTERDAY_OPEN_THREAD'); ?>: <strong><?php echo intval($this->yesterdayopen); ?></strong><span class="divider">|</span>
+							<?php echo JText::_('COM_KUNENA_STAT_TODAY_TOTAL_ANSWER'); ?>: <strong><?php echo intval($this->todayanswer); ?></strong> <span class="divider">|</span> <?php echo JText::_('COM_KUNENA_STAT_YESTERDAY_TOTAL_ANSWER'); ?>: <strong><?php echo intval($this->yesterdayanswer); ?></strong></li>
+						</ul>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</div>
+
+<?php
+}
+
---
+++ components/com_kunena/template/default/threads/showcat - ���� (2).php
@@ -0,0 +1,126 @@
+<?php
+
+// Dont allow direct linking
+defined ( '_JEXEC' ) or die ();
+?>
+<!-- Pathway -->
+<?php $this->displayPathway () ?>
+<!-- / Pathway -->
+
+<?php $this->displaySubCategories () ?>
+<?php if ($this->objCatInfo->headerdesc) : ?>
+<div class="kblock">
+	<div class="kheader">
+		<span class="ktoggler"><a class="ktoggler close" title="<?php echo JText::_('COM_KUNENA_TOGGLER_COLLAPSE') ?>" rel="frontstats_tbody"></a></span>
+		<h2><span><?php echo JText::_('COM_KUNENA_FORUM_HEADER'); ?></span></h2>
+	</div>
+	<div class="kcontainer" id="frontstats_tbody">
+		<div class="kbody">
+			<div class="kfheadercontent">
+				<?php echo KunenaParser::parseBBCode ( $this->headerdesc ); ?>
+			</div>
+		</div>
+	</div>
+</div>
+<?php endif; ?>
+
+<!-- B: List Actions -->
+<table class="klist-actions">
+	<tr>
+		<td class="klist-actions-goto">
+			<a name="forumtop"> </a>
+			<?php echo CKunenaLink::GetSamePageAnkerLink ( 'forumbottom', CKunenaTools::showIcon ( 'kforumbottom', JText::_('COM_KUNENA_GEN_GOTOBOTTOM') ), 'nofollow', 'kbuttongoto') ?>
+		</td>
+		<td class="klist-actions-forum">
+			<?php
+			if (isset ( $this->forum_new ) || isset ( $this->forum_markread ) || isset ( $this->thread_subscribecat )) {
+				echo '<div class="kmessage-buttons-row">';
+				if (isset ( $this->forum_new ))
+					echo $this->forum_new;
+				if (isset ( $this->forum_markread ))
+					echo ' ' . $this->forum_markread;
+				if (isset ( $this->thread_subscribecat ))
+					echo ' ' . $this->thread_subscribecat;
+				echo '</div>';
+			}
+			?>
+		</td>
+		<td class="klist-pages-all">
+			<?php
+			// pagination 1
+			if (count ( $this->messages ) > 0) {
+				$maxpages = 9 - 2; // odd number here (# - 2)
+				echo $pagination = $this->getPagination ( $this->catid, $this->page, $this->totalpages, $maxpages );
+			}
+			?>
+		</td>
+	</tr>
+</table>
+<!-- F: List Actions -->
+
+<?php $this->displayFlat (); ?>
+
+
+
+
+<!-- B: List Actions -->
+<table class="klist-actions">
+	<tr>
+		<td class="klist-actions-goto">
+			<a name="forumtop"> </a>
+			<?php echo CKunenaLink::GetSamePageAnkerLink ( 'forumbottom', CKunenaTools::showIcon ( 'kforumbottom', JText::_('COM_KUNENA_GEN_GOTOBOTTOM') ), 'nofollow', 'kbuttongoto') ?>
+		</td>
+		<td class="klist-actions-forum">
+			<?php
+			if (isset ( $this->forum_new ) || isset ( $this->forum_markread ) || isset ( $this->thread_subscribecat )) {
+				echo '<div class="kmessage-buttons-row">';
+				if (isset ( $this->forum_new ))
+					echo $this->forum_new;
+				if (isset ( $this->forum_markread ))
+					echo ' ' . $this->forum_markread;
+				if (isset ( $this->thread_subscribecat ))
+					echo ' ' . $this->thread_subscribecat;
+				echo '</div>';
+			}
+			?>
+		</td>
+		<td class="klist-pages-all">
+			<?php
+			// pagination 1
+			if (count ( $this->messages ) > 0) {
+				$maxpages = 9 - 2; // odd number here (# - 2)
+				echo $pagination = $this->getPagination ( $this->catid, $this->page, $this->totalpages, $maxpages );
+			}
+			?>
+		</td>
+	</tr>
+</table>
+<!-- F: List Actions -->
+
+
+
+
+<!-- B: List Actions Bottom -->
+
+
+
+
+<!-- B: List Actions Bottom -->
+<div class="kcontainer klist-bottom">
+	<div class="kbody">
+		<div class="kmoderatorslist-jump fltrt">
+				<?php $this->displayForumJump (); ?>
+		</div>
+		<?php if (!empty ( $this->modslist ) ) : ?>
+		<div class="klist-moderators">
+			<?php
+			echo '' . JText::_('COM_KUNENA_GEN_MODERATORS') . ": ";
+			foreach ( $this->modslist as $mod ) {
+				echo CKunenaLink::GetProfileLink ( intval($mod->userid) ) . '&nbsp; ';
+			}
+			?>
+		</div>
+		<?php endif; ?>
+	</div>
+</div>
+<!-- F: List Actions Bottom -->
